000f08c576ee35920cde012197e0cd2fda24ecd9
victor1338 <victorlee1338@gmail.com>
2024-10-30 17:09:38 +0800

    minor

--- Digit/views.py
+++ Digit/views.py
@@ -17,13 +17,6 @@ def digit_reg(req):
     img=req.body
     img=json.loads(img)
     response = urllib.request.urlopen(img['imageUrl']['url'])
-    print()
     img=BytesIO(response.file.read())
-    # print(img['imageUrl']['url'])
-    transform = torchvision.transforms.Compose([
-    torchvision.transforms.ToTensor(),
-    torchvision.transforms.Grayscale(),
-    torchvision.transforms.Resize(28)
-    ])
     predition=predict(img)
     return Response({ "prediction": predition[0], "probability": predition[1]})
